8e1eafeac1a09b60d0f84d2672312b9404ebfba7
Diego Balvin <diego.balvin@gmail.com>
2019-02-24 16:33:32 -0800

    Added checking size validation and defined functions

--- sudoku.rb
+++ sudoku.rb
@@ -7,7 +7,25 @@ puts "such that each row, column, and 3x3 subgrid contains exactly"
 puts "one instance of each number 1-9"
 puts "************************"
 
-puts "Please enter a 9x9 sudoku board below in the form of a valid 2D int array: "
+puts "Please enter a 9x9 sudoku board below in the form of a valid 2D int array, 0 representing empty cells: "
 input = gets.chomp
-array = YAML.load(input)
-array.flatten
+board = YAML.load(input)
+board.flatten
+
+def check_valid_size(board)
+  return false if board.length != 9 # 9 rows
+  board.each do |row|
+    return false if row.length != 9 # 9 columns in each row
+  end
+  return true
+end
+
+def sudoku_solve(board){
+  # Logic to solve the sudoku board. Use recursive backtracking
+end
+
+if check_valid_size(board)
+  sudoku_solve(board)
+else
+  puts "Invalid Board Size... try again!"
+end
